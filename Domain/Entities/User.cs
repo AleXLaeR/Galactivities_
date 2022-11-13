d98b0ac99efcfc2685911389cd30f5609fc8991a
Alexander Lazarenko <ferrosful@gmail.com>
2022-11-13 11:44:48 +0200

    Added Identity User model and changed db context to support identity.

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities;
+
+public class User : IdentityUser
+{
+    public string DisplayName { get; set; }
+
+    public string Biography { get; set; }
+}
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,9 +1,11 @@
 ﻿using Domain;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
 
-public class DataContext : DbContext
+public class DataContext : IdentityDbContext<User>
 {
     public DataContext(DbContextOptions options) : base(options)
     {
