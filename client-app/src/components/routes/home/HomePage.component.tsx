8507151fe9cc6fa68cdce4667bc19075395ce56d
Alexander Lazarenko <ferrosful@gmail.com>
2022-11-22 19:54:12 +0200

    Added conditional user display.

--- client-app/src/components/routes/home/HomePage.component.tsx
+++ client-app/src/components/routes/home/HomePage.component.tsx
@@ -1,27 +1,43 @@
-import {Button, Container, Header, Image, Segment} from "semantic-ui-react";
-import {Link} from "react-router-dom";
-import {ROUTES} from "../../../utils/contants.utils";
-
 import "./HomePage.styles.scss";
 
-const HomePage = () => (
-    <Segment inverted textAlign='center' vertical className='masthead'>
-        <Container text>
-            <Header inverted as='h1'>
-                <Image
-                    size='massive'
-                    src='assets/logo.png'
-                    alt='logo'
-                    style={{marginBottom: '.75rem'}}
-                />
-                Reactivities
-            </Header>
-            <Header as='h2' inverted content='Welcome to Reactivities' />
-            <Button as={Link} to={ROUTES.ACCOUNT.LOGIN} size='huge' inverted>
-                Login!
-            </Button>
-        </Container>
-    </Segment>
-);
+import { useMobXStore } from "../../../app/stores/root.store";
+import { observer } from "mobx-react-lite";
 
-export default HomePage;
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../../utils/contants.utils";
+
+import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+
+const HomePage = () => {
+    const { userStore } = useMobXStore();
+
+    return (
+        <Segment inverted textAlign='center' vertical className='masthead'>
+            <Container text>
+                <Header inverted as='h1'>
+                    <Image
+                        size='massive'
+                        src='assets/logo.png'
+                        alt='logo'
+                        style={{marginBottom: '.75rem'}}
+                    />
+                    Reactivities
+                </Header>
+                {(userStore.isLoggedIn) ? (
+                    <>
+                        <Header as='h2' inverted content='Welcome to Reactivities'/>
+                        <Button as={Link} to={ROUTES.ACTIVITIES.LIST} size='huge' inverted>
+                            Go to Activities!
+                        </Button>
+                    </>
+                ) : (
+                    <Button as={Link} to={ROUTES.ACCOUNT.LOGIN} size='huge' inverted>
+                        Login!
+                    </Button>
+                )}
+            </Container>
+        </Segment>
+    );
+};
+
+export default observer(HomePage);
