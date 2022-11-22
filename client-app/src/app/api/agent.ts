7dd91a4c35d208151fdbd60e4eefef5202899c88
Alexander Lazarenko <ferrosful@gmail.com>
2022-11-22 20:18:01 +0200

    Added axios baseUrl back after removing it.

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -16,6 +16,8 @@ const sleep = (delay: number) => {
     })
 }
 
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
 axios.interceptors.response.use(async response => {
     await sleep(1000);
     return response;
