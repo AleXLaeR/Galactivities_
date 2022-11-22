eeedb25541cdd59e951c0c159e95bca472893ae6
Alexander Lazarenko <ferrosful@gmail.com>
2022-11-22 17:44:09 +0200

    Made filter column sticky

--- client-app/src/components/activity/dashboard/ActivityDashboard.component.tsx
+++ client-app/src/components/activity/dashboard/ActivityDashboard.component.tsx
@@ -1,8 +1,14 @@
-import { Container, Grid } from 'semantic-ui-react';
+import {Container, Grid} from 'semantic-ui-react';
 import ActivityList from './ActivityList.component';
 
 import { observer } from "mobx-react-lite";
 import ActivityFilters from "./filters/ActivityFilters.component";
+
+const FilterStyles = {
+    top: '5rem',
+    position: 'sticky',
+    alignSelf: 'flex-start',
+}
 
 const ActivityDashboard = () => (
     <Container style={{marginTop: '6rem'}}>
@@ -10,8 +16,8 @@ const ActivityDashboard = () => (
             <Grid.Column width='10'>
                 <ActivityList/>
             </Grid.Column>
-            <Grid.Column width='6'>
-                <ActivityFilters />
+            <Grid.Column width='6' style={FilterStyles}>
+                <ActivityFilters/>
             </Grid.Column>
         </Grid>
     </Container>
