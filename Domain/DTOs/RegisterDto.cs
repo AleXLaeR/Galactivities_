0928cd211bf265ff10ef27660ce86331c105a5e6
Alexander Lazarenko <ferrosful@gmail.com>
2022-11-22 10:01:43 +0200

    Added basic auth DTOs.

---
+++ Domain/DTOs/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.DTOs;
+
+public class RegisterDto
+{
+    public string DisplayName { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public string Username { get; set; }
+}
---
+++ Domain/DTOs/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.DTOs;
+
+public class UserDto
+{
+    public string DisplayName { get; set; }
+
+    public string Token { get; set; }
+
+    public string Username { get; set; }
+    
+    public string? ImageUri { get; set; }
+}
