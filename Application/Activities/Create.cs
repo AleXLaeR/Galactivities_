03dd2f03cf120a9421aefbe8fba9739a71e806d8
Alexander Lazarenko <ferrosful@gmail.com>
2022-11-24 15:49:35 +0200

    Made it so the creator of an activity will be its host.

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,7 +1,10 @@
 ﻿using Domain;
 using Domain.Entities;
+using Domain.Entities.Junctions;
 using FluentResults;
+using Infrastructure.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities;
@@ -16,14 +19,26 @@ public class Create
     public class Handler : IRequestHandler<Command, Result<Unit>>
     {
         private readonly DataContext _context;
+        private readonly IUserAccessor _userAccessor;
 
-        public Handler(DataContext context)
+        public Handler(DataContext context, IUserAccessor userAccessor)
         {
             _context = context;
+            _userAccessor = userAccessor;
         }
         
         public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
         {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.UserName == _userAccessor.GetUsername());
+
+            request.Activity.Attendees.Add(new ActivityAttendee
+            {
+                User = user!,
+                Activity = request.Activity,
+                IsHost = true,
+            });
+            
             _context.Activities.Add(request.Activity);
             
             var resultIsSuccess = await _context.SaveChangesAsync(cancellationToken) > 0;
